6a9e2b7a00959e4a6e033eece185a189ea771d88
Gilberto De Leon <gdleonq@gmail.com>
2023-09-22 22:00:37 -0700

    doing it using Kubernetes

--- main.tf
+++ main.tf
@@ -1,28 +1,30 @@
-# Define your variables here
+# Will run Kubernetes with 2 Nodes running NGNIX. They can be accesible from internet since teh type is Load Balancer
+#This is main.tf V1.2
+
+# Define variables here
+variable "resource_group_name" {
+  description = "Name of the Azure resource group"
+  default     = "TerraformRG"
+}
 
 variable "location" {
   description = "Azure region for resources"
   default     = "westus3"
 }
 
-variable "address_space" {
-  description = "Address space for the virtual network"
-  default     = ["10.0.0.0/16"]
+variable "aks_cluster_name" {
+  description = "Name of the AKS cluster"
+  default     = "my-aks-cluster"
 }
 
-variable "subnet_address_prefix" {
-  description = "Address prefix for the subnet"
-  default     = "10.0.1.0/24"
+variable "aks_node_count" {
+  description = "Number of AKS nodes"
+  default     = 2
 }
 
-variable "source_ip_prefix" {
-  description = "Source IP prefix for security group rule"
-  default     = "97.0.0.0/8"
-}
-
-variable "resource_group_name" {
-  description = "Name of the Azure resource group"
-  default     = "TerraformRG"
+variable "aks_node_size" {
+  description = "Size of AKS nodes"
+  default     = "Standard_B2s"
 }
 
 # Provider configuration
@@ -36,139 +38,91 @@ resource "azurerm_resource_group" "terraform" {
   location = var.location
 }
 
-# Define the virtual network
-resource "azurerm_virtual_network" "nginx_vnet" {
-  name                = "NGINX_VNET"
-  address_space       = var.address_space
+# Create an Azure Kubernetes Service (AKS) cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
   location            = azurerm_resource_group.terraform.location
   resource_group_name = azurerm_resource_group.terraform.name
-}
+  dns_prefix          = var.aks_cluster_name
 
-# Define the subnet
-resource "azurerm_subnet" "nginx_subnet" {
-  name                 = "nginx_subnet"
-  resource_group_name  = azurerm_resource_group.terraform.name
-  virtual_network_name = azurerm_virtual_network.nginx_vnet.name
-  address_prefixes     = [var.subnet_address_prefix]
-}
+  default_node_pool {
+    name       = "default"
+    node_count = var.aks_node_count
+    vm_size    = var.aks_node_size
+  }
 
-# Define the network security group
-resource "azurerm_network_security_group" "nsg_only_from_centurylink" {
-  name                = "NSG_Only_from_Centurylink"
-  location            = azurerm_resource_group.terraform.location
-  resource_group_name = azurerm_resource_group.terraform.name
-
-  security_rule {
-    name                       = "http"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = var.source_ip_prefix
-    destination_address_prefix = "*"
+  tags = {
+    Environment = "Dev"
   }
 }
 
-# Define public IPs
-resource "azurerm_public_ip" "lb" {
-  name                = "LB_PublicIP"
-  location            = azurerm_resource_group.terraform.location
-  resource_group_name = azurerm_resource_group.terraform.name
-  allocation_method   = "Dynamic"
-}
+# Create a Kubernetes Deployment for NGINX
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "nginx-deployment"
+  }
 
-resource "azurerm_public_ip" "nic" {
-  name                = "VM_NICPublicIP"
-  location            = azurerm_resource_group.terraform.location
-  resource_group_name = azurerm_resource_group.terraform.name
-  allocation_method   = "Dynamic"
-}
+  spec {
+    replicas = 2
 
-# Define the network interface
-resource "azurerm_network_interface" "example" {
-  name                = "VM_NIC"
-  location            = azurerm_resource_group.terraform.location
-  resource_group_name = azurerm_resource_group.terraform.name
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
 
-  ip_configuration {
-    name                          = "myNICConfig"
-    subnet_id                     = azurerm_subnet.nginx_subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id           = azurerm_public_ip.nic.id
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+
+      spec {
+        container {
+          image = "nginx:latest"
+          name  = "nginx"
+
+          ports {
+            container_port = 80
+          }
+        }
+      }
+    }
   }
 }
 
-# Define the virtual machine
-resource "azurerm_linux_virtual_machine" "nginxvm" {
-  name                = "NGINXVM"
-  location            = azurerm_resource_group.terraform.location
-  resource_group_name = azurerm_resource_group.terraform.name
-  network_interface_ids = [azurerm_network_interface.example.id]
-
-  size                 = "Standard_B1s"
-  admin_username       = var.admin_username
-  admin_password       = var.admin_password
-
-  os_disk {
-    name                 = "myOsDisk"
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+# Create a Kubernetes Service of type LoadBalancer to expose NGINX
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx-service"
   }
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-gen2"
-    version   = "latest"
-  }
+  spec {
+    selector = {
+      app = "nginx"
+    }
 
-  custom_data = filebase64("ngnixinstall.sh")
+    port {
+      port        = 80
+      target_port = 80
+    }
 
-  disable_password_authentication = false
-}
-
-# Define the load balancer
-resource "azurerm_lb" "lb_external" {
-  name                = "LB_External"
-  location            = azurerm_resource_group.terraform.location
-  resource_group_name = azurerm_resource_group.terraform.name
-  sku                 = "Basic"
-
-  frontend_ip_configuration {
-    name                          = "publicIPAddress"
-    public_ip_address_id          = azurerm_public_ip.lb.id
+    type = "LoadBalancer"
   }
 }
 
-# Define the load balancer backend pool
-resource "azurerm_lb_backend_address_pool" "example" {
-  name              = "backendPool"
-  loadbalancer_id   = azurerm_lb.lb_external.id
+# Output for the kube_config block to access the AKS cluster
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks.kube_config.0
 }
 
-# Define the load balancer probe
-resource "azurerm_lb_probe" "http_probe" {
-  name              = "httpProbe"
-  protocol          = "Tcp"
-  port              = "80"
-  loadbalancer_id   = azurerm_lb.lb_external.id
+# Output for the AKS cluster's credentials
+output "cluster_credentials" {
+  value = azurerm_kubernetes_cluster.aks
 }
 
-# Define the load balancer rule
-resource "azurerm_lb_rule" "http" {
-  name                           = "http"
-  loadbalancer_id                 = azurerm_lb.lb_external.id
-  frontend_ip_configuration_name  = "publicIPAddress"
-  backend_address_pool_ids        = [azurerm_lb_backend_address_pool.example.id]
-  probe_id                        = azurerm_lb_probe.http_probe.id
-  protocol                        = "Tcp"
-  frontend_port                   = 80
-  backend_port                    = 80
-}
-
-# Output
-output "public_ip_address" {
-  value = azurerm_public_ip.lb.ip_address
+# Output for the public IP address of the NGINX service
+output "nginx_service_public_ip" {
+  value = kubernetes_service.nginx.status.0.load_balancer_ingress.0.ip
 }
