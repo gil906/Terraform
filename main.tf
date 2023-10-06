a0f67dac2772b5cae2c22d2e452b7cf07a2a7405
Gilbert De Leon <gdleonq@gmail.com>
2023-10-05 23:12:04 -0700

    improving main.tf

--- main.tf
+++ main.tf
@@ -1,39 +1,28 @@
-resource "kubernetes_secret" "tls" {
-  for_each = var.enable_https ? { "tls-secret" = var.tls_secret_name } : {}
-  
-  metadata {
-    name = each.key
-  }
-
-  data = {
-    "tls.crt" = file("path/to/tls.crt")
-    "tls.key" = file("path/to/tls.key")
-  }
+# main.tf
+provider "azurerm" {
+  features {}
 }
 
-resource "kubernetes_ingress" "nginx" {
-  for_each = var.enable_https ? { "nginx-ingress" = var.tls_secret_name } : {}
+module "aks_cluster" {
+  source = "./aks"
+}
 
-  metadata {
-    name = each.key
-  }
+module "nginx_resources" {
+  source = "./nginx"
+}
 
-  spec {
-    tls {
-      secret_name = each.value
-    }
+module "helm_init" {
+  source = "./helm"
+}
 
-    rule {
-      host = "your-domain.com"
-      http {
-        path {
-          path = "/"
-          backend {
-            service_name = kubernetes_service.nginx.metadata[0].name
-            service_port = kubernetes_service.nginx.spec[0].port[0].port
-          }
-        }
-      }
-    }
-  }
+output "kube_config" {
+  value = module.aks_cluster.kube_config
+}
+
+output "cluster_credentials" {
+  value = module.aks_cluster.aks
+}
+
+output "nginx_service_public_ip" {
+  value = module.nginx_resources.nginx_service_public_ip
 }
