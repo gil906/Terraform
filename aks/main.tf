051613f68f0bd753c399825aa16c124bd051e51e
Gilbert De Leon <gdleonq@gmail.com>
2023-10-11 00:01:32 -0700

    Update main.tf
    Adding Network Policy, service principal, RBAC, Linux profile

--- aks/main.tf
+++ aks/main.tf
@@ -19,4 +19,59 @@ resource "azurerm_kubernetes_cluster" "aks" {
   tags = {
     Environment = "Dev"
   }
+
+  # Enable network policy for AKS (optional)
+  network_profile {
+    network_plugin = "azure"
+    network_policy = "azure"
+  }
+
+  service_principal {
+    client_id     = "your-service-principal-client-id"
+    client_secret = "your-service-principal-client-secret"
+  }
+
+  addon_profile {
+    kube_dashboard {
+      enabled = true
+    }
+
+    oms_agent {
+      enabled                    = true
+      log_analytics_workspace_id = "your-log-analytics-workspace-id"
+    }
+
+    http_application_routing {
+      enabled = true
+    }
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  aad_profile {
+    enabled = true
+    admin_group_object_ids = ["your-admin-group-object-id"]
+  }
+
+  linux_profile {
+    admin_username = "adminuser"
+    ssh_key {
+      key_data = "your-ssh-public-key"
+    }
+  }
+
+  tags = {
+    Environment = "Dev"
+  }
+}
+
+# Output for AKS cluster configuration
+output "aks_cluster_id" {
+  value = azurerm_kubernetes_cluster.aks.id
+}
+
+output "aks_cluster_fqdn" {
+  value = azurerm_kubernetes_cluster.aks.fqdn
 }
